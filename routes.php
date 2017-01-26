8b6166ac6f7963acc56cd5d6040e3b2a6043c2bf
Eugene Sinitsyn <phollyolly@gmail.com>
2017-01-26 23:21:05 +0300

    delete routes file from root dir

--- routes.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-$router->get('', 'PagesController@index');
-$router->get('about', 'PagesController@about');
-$router->get('contact', 'PagesController@contact');
-
-$router->get('tasks', 'TaskController@index');
-$router->post('tasks', 'TaskController@store');
